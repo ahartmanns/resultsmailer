f64e622eb00d2c52313fed0cec136d5ddeb8e3ab
Arnd Hartmanns <a.hartmanns@utwente.nl>
2020-06-11 18:05:20 +0200

    Moved to a more advanced JSON-based configuration.

---
+++ Configuration.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace ResultsMailer
+{
+	sealed class Configuration
+	{
+		public sealed class DataConfiguration
+		{
+			[JsonPropertyName("file")]
+			public string File { get; set; }
+
+			[JsonPropertyName("columns")]
+			public int Columns { get; set; } = -1;
+
+			[JsonPropertyName("separator")]
+			public char Separator { get; set; } = ';';
+
+			[JsonPropertyName("header-rows")]
+			public int HeaderRows { get; set; } = 0;
+
+			[JsonPropertyName("to-column")]
+			public int ToColumn { get; set; }
+
+			[JsonPropertyName("exclude-if-empty-column")]
+			public int ExcludeIfEmptyColumn { get; set; }
+
+			public bool Check(Action<string> reportError)
+			{
+				if(string.IsNullOrWhiteSpace(File))
+				{
+					reportError("Missing or empty data file name.");
+					return false;
+				}
+				if(Columns < 0)
+				{
+					reportError("Invalid number of columns or number of columns not specified.");
+					return false;
+				}
+				return true;
+			}
+		}
+
+		public sealed class TemplateConfiguration
+		{
+			[JsonPropertyName("file")]
+			public string File { get; set; }
+
+			[JsonPropertyName("subject")]
+			public string Subject { get; set; }
+
+			public bool Check(Action<string> reportError)
+			{
+				if(string.IsNullOrWhiteSpace(File))
+				{
+					reportError("Missing or empty template file name.");
+					return false;
+				}
+				if(string.IsNullOrWhiteSpace(Subject))
+				{
+					reportError("Missing or empty email subject.");
+					return false;
+				}
+				return true;
+			}
+		}
+
+		public sealed class EmailAddress
+		{
+			[JsonPropertyName("name")]
+			public string Name { get; set; }
+
+			[JsonPropertyName("email")]
+			public string EMail { get; set; }
+
+			public bool Check(Action<string> reportError)
+			{
+				if(string.IsNullOrWhiteSpace(EMail))
+				{
+					reportError("Missing or invalid email address.");
+					return false;
+				}
+				return true;
+			}
+		}
+
+		public sealed class ServerConfiguration
+		{
+			[JsonPropertyName("host")]
+			public string Host { get; set; }
+
+			[JsonPropertyName("port")]
+			public int Port { get; set; }
+
+			[JsonPropertyName("login")]
+			public string Login { get; set; }
+
+			[JsonPropertyName("password")]
+			public string Password { get; set; }
+
+			public bool Check(Action<string> reportError)
+			{
+				if(string.IsNullOrWhiteSpace(Host))
+				{
+					reportError("Missing or invalid server hostname.");
+					return false;
+				}
+				if(string.IsNullOrWhiteSpace(Login))
+				{
+					reportError("Missing or invalid server login name.");
+					return false;
+				}
+				if(string.IsNullOrWhiteSpace(Password))
+				{
+					reportError("Missing or invalid server password.");
+					return false;
+				}
+				return true;
+			}
+		}
+
+		[JsonPropertyName("log")]
+		public string Log { get; set; }
+
+		[JsonPropertyName("data")]
+		public DataConfiguration Data { get; set; }
+
+		[JsonPropertyName("template")]
+		public TemplateConfiguration Template { get; set; }
+
+		[JsonPropertyName("sender")]
+		public EmailAddress Sender { get; set; }
+
+		[JsonPropertyName("reply-to")]
+		public EmailAddress ReplyTo { get; set; }
+
+		[JsonPropertyName("server")]
+		public ServerConfiguration Server { get; set; }
+
+		public bool Check(Action<string> reportError)
+		{
+			if(string.IsNullOrWhiteSpace(Log))
+			{
+				reportError("Missing or empty log file name.");
+				return false;
+			}
+			if(Data == null)
+			{
+				reportError("Missing data configuration.");
+				return false;
+			}
+			if(Template == null)
+			{
+				reportError("Missing template configuration.");
+				return false;
+			}
+			if(Sender == null)
+			{
+				reportError("Missing sender configuration.");
+				return false;
+			}
+			if(Server == null)
+			{
+				reportError("Missing server configuration.");
+				return false;
+			}
+			return Data.Check(reportError)
+				&& Template.Check(reportError)
+				&& Sender.Check(reportError)
+				&& (ReplyTo?.Check(reportError) ?? true)
+				&& Server.Check(reportError);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json;
 using MailKit.Net.Smtp;
 using MimeKit;
 using CI = System.Globalization.CultureInfo;
@@ -12,16 +13,41 @@ namespace ResultsMailer
 	{
 		static void Main(string[] args)
 		{
+			// Get and check arguments
 			string command = args[0];
-			string csvFileName = args[1];
-			string templateFileName = args[2];
-			string settingsFileName = args[3];
-			string logFileName = args[4];
+			if(command != "send" && command != "log")
+			{
+				Console.WriteLine("Error: Unsupported command.");
+				return;
+			}
+			string configFile = args[1];
+			if(!File.Exists(configFile))
+			{
+				Console.WriteLine("Error: Configuration file not found.");
+				return;
+			}
+			string relativeDir = Path.GetDirectoryName(Path.GetFullPath(configFile));
+
+			// Load configuration
+			Configuration config;
+			try
+			{
+				config = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(configFile));
+			}
+			catch(JsonException je)
+			{
+				Console.WriteLine("Error reading configuration: " + je.Message);
+				return;
+			}
+			if(!config.Check(error => Console.WriteLine("Error: " + error))) return;
 
 			// Load and parse CSV file
 			Console.Write("Loading CSV...");
-			string[] csvLines = File.ReadAllLines(csvFileName, Encoding.UTF8);
-			string[][] csvCells = new string[csvLines.Length][];
+			string csvFilePath = Path.IsPathFullyQualified(config.Data.File) ? config.Data.File : Path.Combine(relativeDir, config.Data.File);
+			string[] csvAllLines = File.ReadAllLines(csvFilePath, Encoding.UTF8);
+			var csvLines = new Span<string>(csvAllLines, config.Data.HeaderRows, csvAllLines.Length - config.Data.HeaderRows);
+			var csvCells = new Span<string[]>(new string[csvLines.Length][]);
+			int rowCount = 0;
 			for(int i = 0; i < csvLines.Length; ++i)
 			{
 				string line = csvLines[i];
@@ -31,99 +57,108 @@ namespace ResultsMailer
 				for(int j = 0; j < line.Length; ++j)
 				{
 					char c = line[j];
-					switch(c)
+					if(c == config.Data.Separator)
 					{
-						case ',':
-							if(inString)
-							{
-								cell.Append(c);
-							}
-							else
-							{
-								cells.Add(cell.ToString());
-								cell.Clear();
-							}
-							break;
-						case '"':
-							if(inString)
-							{
-								if(j < line.Length - 1 && line[j + 1] == '"')
-								{
-									// Escaped quote
-									cell.Append('"');
-									++j;
-								}
-								else
-								{
-									// End of string
-									inString = false;
-								}
-							}
-							else
-							{
-								// Beginning of string
-								inString = true;
-							}
-							break;
-						default:
+						if(inString)
+						{
 							cell.Append(c);
-							break;
+						}
+						else
+						{
+							cells.Add(cell.ToString());
+							cell.Clear();
+						}
+					}
+					else if(c == '"')
+					{
+						if(inString)
+						{
+							if(j < line.Length - 1 && line[j + 1] == '"')
+							{
+								// Escaped quote
+								cell.Append('"');
+								++j;
+							}
+							else
+							{
+								// End of string
+								inString = false;
+							}
+						}
+						else
+						{
+							// Beginning of string
+							inString = true;
+						}
+					}
+					else
+					{
+						cell.Append(c);
 					}
 				}
-				if(cell.Length != 0) cells.Add(cell.ToString());
-				csvCells[i] = cells.ToArray();
+				/*if(cell.Length != 0)*/ cells.Add(cell.ToString());
+				if(cells.Count != config.Data.Columns)
+				{
+					Console.WriteLine();
+					Console.WriteLine("Error: Expected " + config.Data.Columns.ToString(CI.InvariantCulture) + " columns in data file line " + (i + 1 + config.Data.HeaderRows).ToString(CI.InvariantCulture) + ", found " + cells.Count.ToString(CI.InvariantCulture) + ".");
+					return;
+				}
+				if(config.Data.ExcludeIfEmptyColumn <= 0 || cells[config.Data.ExcludeIfEmptyColumn - 1].Length != 0)
+				{
+					csvCells[rowCount] = cells.ToArray();
+					++rowCount;
+				}
 			}
-			Console.WriteLine(" " + csvCells.Length.ToString(CI.InvariantCulture) + " lines loaded.");
+			csvCells = csvCells.Slice(0, rowCount);
+			Console.WriteLine(" " + csvCells.Length.ToString(CI.InvariantCulture) + " entries loaded.");
 
 			// Load and instantiate template
 			Console.Write("Preparing messages...");
-			string template = File.ReadAllText(templateFileName, Encoding.UTF8);
+			string templateFilePath = Path.IsPathFullyQualified(config.Template.File) ? config.Template.File : Path.Combine(relativeDir, config.Template.File);
+			string template = File.ReadAllText(templateFilePath, Encoding.UTF8);
 			int lineEnd = template.IndexOf('\n');
-			string subject = template[..lineEnd].Trim();
-			template = template[(lineEnd + 1)..];
-			lineEnd = template.IndexOf('\n');
-			int toIndex = int.Parse(template[..lineEnd].Trim(), CI.InvariantCulture) - 1;
-			template = template[(lineEnd + 1)..];
 			string[] messages = new string[csvCells.Length];
 			string[] tos = new string[csvCells.Length];
 			string[] logs = new string[csvCells.Length];
 			for(int i = 0; i < csvCells.Length; ++i)
 			{
 				string[] cell = csvCells[i];
-				tos[i] = cell[toIndex];
+				tos[i] = cell[config.Data.ToColumn - 1];
 				string message = template;
-				for(int j = cell.Length - 1; j >= 0; --j) // we go backwards so that we replace e.g. %10 with column 10, not column 1
+				for(int j = cell.Length - 1; j >= 0; --j) // we go backwards so that we replace e.g. %10 with column 10, not with column 1 and leaving a 0
 				{
 					message = message.Replace("%" + (j + 1).ToString(CI.InvariantCulture), cell[j]);
 				}
 				if(message.Contains('%'))
 				{
 					Console.WriteLine();
-					Console.WriteLine("Error: Missing column or \"%\" symbol in CSV file line " + (i + 1).ToString(CI.InvariantCulture) + ".");
+					Console.WriteLine("Error: Missing column or \"%\" symbol in data file line " + (i + 1).ToString(CI.InvariantCulture) + ".");
 					return;
 				}
 				messages[i] = message;
 			}
 			Console.WriteLine(" " + messages.Length.ToString(CI.InvariantCulture) + " messages prepared.");
 
-			// Load settings
-			Console.Write("Loading server configuration...");
-			string[] settingsLines = File.ReadAllLines(settingsFileName, Encoding.UTF8);
-			string senderName = settingsLines[0];
-			string senderEmail = settingsLines[1];
-			string server = settingsLines[2];
-			int port = int.Parse(settingsLines[3]);
-			string user = settingsLines[4];
-			string password = settingsLines[5];
-			Console.WriteLine(" done.");
-
 			// Create and send emails
-			Console.WriteLine("Sending:");
-			using(var messageLogWriter = new StreamWriter(File.OpenWrite(logFileName)) { AutoFlush = true })
+			if(command == "send")
+			{
+				Console.Write("Confirm by typing 'yes' to send " + messages.Length.ToString(CI.InvariantCulture) + " e-mails: ");
+				string line = Console.ReadLine();
+				if(line != "yes")
+				{
+					Console.WriteLine("Cancelled.");
+					return;
+				}
+			}
+			Console.WriteLine((command == "log" ? "Logging" : "Sending") + " " + messages.Length.ToString(CI.InvariantCulture) + " messages:");
+			var senderAddress = new MailboxAddress(config.Sender.Name, config.Sender.EMail);
+			var replyToAddress = config.ReplyTo == null ? null : new MailboxAddress(config.ReplyTo.Name, config.ReplyTo.EMail);
+			string logFilePath = Path.IsPathFullyQualified(config.Log) ? config.Log : Path.Combine(relativeDir, config.Log);
+			using(var messageLogWriter = new StreamWriter(File.Open(logFilePath, FileMode.Create, FileAccess.Write, FileShare.Read), Encoding.UTF8) { AutoFlush = true })
 			using(var client = new SmtpClient())
 			{
-				client.Connect(server, port);
-				client.Authenticate(user, password);
+				client.Connect(config.Server.Host, config.Server.Port);
+				client.Authenticate(config.Server.Login, config.Server.Password);
 				for(int i = 0; i < messages.Length; ++i)
 				{
 					string message = messages[i];
@@ -132,14 +167,23 @@ namespace ResultsMailer
 
 					// Create email
 					var mail = new MimeMessage();
-					mail.From.Add(new MailboxAddress(senderName, senderEmail));
+					mail.From.Add(senderAddress);
 					mail.To.Add(new MailboxAddress((string)null, to));
-					mail.Subject = subject;
+					if(replyToAddress != null) mail.ReplyTo.Add(replyToAddress);
+					mail.Subject = config.Template.Subject;
 					mail.Body = new TextPart() { Text = message };
 
 					// Send email
 					if(command == "send") client.Send(mail);
-					messageLogWriter.WriteLine(to + ": " + message.Replace("\r", "").Replace("\n", "/"));
+					messageLogWriter.WriteLine("To: " + mail.To.ToString());
+					messageLogWriter.WriteLine("From: " + mail.From.ToString());
+					if(replyToAddress != null) messageLogWriter.WriteLine("Reply-To: " + mail.ReplyTo.ToString());
+					messageLogWriter.WriteLine("Subject: " + mail.Subject);
+					messageLogWriter.WriteLine(">>>");
+					messageLogWriter.WriteLine(message);
+					messageLogWriter.WriteLine("<<<");
+					messageLogWriter.WriteLine();
+					messageLogWriter.WriteLine();
 				}
 				client.Disconnect(true);
 			}
